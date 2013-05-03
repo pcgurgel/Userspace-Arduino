d222e1011fc1f5d670bddb45d1d0c2d65eff6f50
Anuj Deshpande <anujdeshpande92@gmail.com>
2013-05-03 23:21:09 +0530

    added beagle-bone to boards, core/

---
+++ cores/beaglebone-black/wiring.c
@@ -0,0 +1,54 @@
+/*
+  wiring.c - Partial implementation of the Wiring API for the ATmega8.
+  Part of Arduino - http://www.arduino.cc/
+
+  Copyright (c) 2005-2006 David A. Mellis
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General
+  Public License along with this library; if not, write to the
+  Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+  Boston, MA  02111-1307  USA
+
+  $Id$
+*/
+
+#include <stdio.h>
+
+unsigned long millis()
+{
+
+	printf("millis() called!\n");
+
+	return 1;
+}
+
+unsigned long micros() {
+	printf("micros() called!\n");
+	return 1;
+}
+
+void delay(unsigned long ms)
+{
+	usleep(ms*1000);
+}
+
+/* Delay for the given number of microseconds.  Assumes a 8 or 16 MHz clock. */
+void delayMicroseconds(unsigned int us)
+{
+	usleep(us);
+}
+
+void init()
+{
+	printf("init() called!\n");
+}
