5a4869a06daa0c2e187278bae6073fd8bca8412d
Anuj Deshpande <anujdeshpande92@gmail.com>
2013-07-06 21:01:35 +0530

    Temp commit

--- generic_functions.c
+++ generic_functions.c
@@ -7,85 +7,87 @@
 #include <unistd.h>
 #include <time.h>
 
-int sysfs_read(char* path, char* filename)
+int sysfs_read(const char* path, const char* filename) 
 {
 		FILE* fd;
 		char buf[MAX_BUF];
-		int i;
 		char value[MAX_BUF];
 		snprintf(buf, sizeof(buf),"%s%s",path,filename);
 		fd=fopen(buf,"r");
-		fgets(value,10,fd);
-		printf("%s",value);
-		fclose(fd);
+		if(fd==NULL){
+				printf("\nError opening file");
+				return -1;
+		}
 
+		fgets(value,10,fd);
+		fclose(fd);
+        return 0;
 }
 
-int sysfs_write(char* path, char* filename,char* value)
+int sysfs_write(const char* path, const char* filename,int value) 
 {
-
 		FILE* fd;
 		char buf[MAX_BUF];
-		int i;
 		snprintf(buf, sizeof(buf),"%s%s",path,filename);
 		fd=fopen(buf,"w");
-		fprintf(fd,"%s",value);
-		printf("%s",value);
+		if(fd==NULL){
+				printf("\nError opening file");
+				return -1;
+		}
+		fprintf(fd,"%d",value);
 		fclose(fd);
-
+		return 0;
 }
-int gpio_export(int gpio_pin)
+int gpio_export(uint32_t gpio_pin) 
 {
 		FILE* fd;
-		int i;
 		fd=fopen("/sys/class/gpio/export","w");
+		if(fd==NULL){
+				printf("\nError opening file");
+				return -1;
+		}
+
 		fprintf(fd,"%d",gpio_pin);
 		fclose(fd);
 		return gpio_pin;
 }
 
-int gpio_unexport(int gpio_pin)
+int gpio_unexport(uint32_t gpio_pin) 
 {
 		FILE* fd;
-		int i;
 		fd=fopen("/sys/class/gpio/unexport","w");
+		if(fd==NULL){
+				printf("\nError opening file");
+				return -1;
+		}
 		fprintf(fd,"%d",gpio_pin);
 		fclose(fd);
 		return gpio_pin;
 }
-
 
 void delay(unsigned long ms)
 {
-  usleep(ms*1000);
+		usleep(ms*1000);
 }
+
 void delayMicroseconds(unsigned int us)
 {
-  usleep(us);
+		usleep(us);
+}
+int gpio_setdirection(uint32_t gpio_pin, const char* direction)
+{
+		FILE* fd;
+		fd=fopen("/sys/class/gpio/unexport","w");
+		if(fd==NULL){
+				printf("\nError opening file");
+				return -1;
+		}
+		
+		fclose(fd);
+		return gpio_pin;
 }
 
 int main()
 {
-		char ch[MAX_BUF];
-		int i;
-		/* gpio_unexport(26); */
-		/* gpio_export(26); */
-		/* sysfs_write("/sys/class/gpio/gpio26/","direction","out"); */
-		/* sysfs_write("/sys/class/gpio/gpio26/","value","0"); */
-		/* sysfs_write("/sys/class/gpio/gpio26/","value","1"); */
-				
-		sysfs_write("/sys/class/leds/beaglebone:green:usr0/","brightness","1");
-		delayMicroseconds(1000000);
-		sysfs_write("/sys/class/leds/beaglebone:green:usr0/","brightness","0");
-		delayMicroseconds(1000000);
-		sysfs_write("/sys/class/leds/beaglebone:green:usr0/","brightness","1");
-		delayMicroseconds(1000000);
-		sysfs_write("/sys/class/leds/beaglebone:green:usr0/","brightness","0");
-		delayMicroseconds(1000000);
-		sysfs_write("/sys/class/leds/beaglebone:green:usr0/","brightness","1");
-		delayMicroseconds(1000000);
-		sysfs_write("/sys/class/leds/beaglebone:green:usr0/","brightness","0");
-		delayMicroseconds(1000000);
 
-		
 }
--- send_exec.sh
+++ send_exec.sh
@@ -1,42 +1,25 @@
 #!/bin/bash
 
-# SETTING UP SSH KEYS FOR YOUR BOARD (Required for password-less login)
-# cd ~/.ssh
-# ssh-keygen -t dsa
-# ssh-copy-id -i ~/.ssh/id_dsa.pub $USERNAME@$IP
-# ssh $USERNAME@$IP
 
 # COMMAND LINE ARGUMENTS
 # ./send_exec.sh arduinofile.ino
 
 # A bash script to helloworld.bin to beagleboard and return the output
+
 IP="192.168.7.2"
 USERNAME="root"
 FILE=$1
-FILE=${FILE%%\.*}
-#echo $FILE
-EXECUTABLE=$FILE.elf
-#echo $EXECUTABLE
-make
+# FILE=${FILE%%\.*}
+# EXECUTABLE=$FILE.elf
 
- echo "ls
- put build-userspace/"$EXECUTABLE"
- exit" > commands.tmp
-
-  sftp -b commands.tmp $USERNAME@$IP
-  ssh $USERNAME@$IP #-t "./"$EXECUTABLE
-
-
-
-# IGNORE THIS
-
-# Installing GNU Netcat from source
-# wget http://kaz.dl.sourceforge.net/project/netcat/netcat/0.7.1/netcat-0.7.1.tar.gz
-# tar xzvf netcat-0.7.1.tar.gz 
-# cd netcat-0.7.1/
-# ./configure 
 # make
-# make check
-# sudo make install
-# make clean
-# sudo apt-get install sshpass
+
+# echo "ls
+#  put build-userspace/"$EXECUTABLE"
+#  exit" > commands.tmp
+
+sftp -b commands.tmp $USERNAME@$IP
+ssh $USERNAME@$IP #-t "./"$EXECUTABLE
+
+
+
