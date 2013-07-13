1a9cb0e394887dfb0ef7f7abfb3af114470b0ef1
Anuj Deshpande <anujdeshpande92@gmail.com>
2013-07-13 23:33:14 +0530

    Add user button input to 7segment
    Remove infinite loop through numbers. Accept user button input to display next number

--- arduino-makefile/examples/7segment/7segment.ino
+++ arduino-makefile/examples/7segment/7segment.ino
@@ -1,43 +1,55 @@
-/*
-7segment
+	
 
-Displays digits on 7 segment display on Bacon Board
-*/
+    /*
+    7segment
+     
+    Displays digits on 7 segment display on Bacon Board
+    */
+     
+    #define LATCH 10
+    #define CLOCK 13
+    #define DATA 11
+    #define pushButton 7
+     
+     
+    /*
+     clk=/sys/class/gpio/gpio2
+     serial=/sys/class/gpio/gpio4
+     latch=/sys/class/gpio/gpio5
+    */
+    const byte ledCharSet[10] = {
+     
+      B00111111,B00000110,B01011011,B01001111,B01100110,B01101101,B01111101,B00000111,B01111111,B01101111
+    };
+     
+    void setup(){
+      // initialise digital pins as inputs
+      pinMode(pushButton, INPUT);
+      pinMode(LATCH, 1);
+      pinMode(CLOCK, 1);
+      pinMode(DATA,1);
+     
+    }
+     
+    int i = 0;
+     
+     
+    void loop(){
+            int buttonState = digitalRead(pushButton);
+            /* Serial.println("Seven segment display test"); */
+            /* print out the state of the button: */
+            printf("%d\n", buttonState);
+            /* delay in between reads for stability */
+            if (!buttonState) {
+                    digitalWrite(LATCH,0);
+                    shiftOut(DATA,CLOCK,MSBFIRST,~(ledCharSet[i]));
+                    digitalWrite(LATCH,1);
+                    i++;
+                    if(i==10){
+                        i = 0;
+                    }
+            delay(250);
+            }
+            delay(10);
+    }
 
-#define LATCH 10 
-#define CLOCK 13
-#define DATA 11
-
-/* 
- clk=/sys/class/gpio/gpio2
- serial=/sys/class/gpio/gpio4
- latch=/sys/class/gpio/gpio5
-*/
-int i;
-const byte ledCharSet[10] = {
-
-  B00111111,B00000110,B01011011,B01001111,B01100110,B01101101,B01111101,B00000111,B01111111,B01101111
-};
-
-void setup(){
-  pinMode(LATCH, 1);
-  pinMode(CLOCK, 1);
-  pinMode(DATA,1);
-
-}
-
-void loop(){
-	 	     i = 0;
-    		 /* Serial.println("Seven segment display test"); */
-			 while(1){
-			 digitalWrite(LATCH,0);
-			 shiftOut(DATA,CLOCK,MSBFIRST,~(ledCharSet[i]));
-      		 digitalWrite(LATCH,1);
-      		 i++;
-			  if(i==10){
-			  	           i = 0;
-	         }
-		      delay(500);
-}
-
-}
