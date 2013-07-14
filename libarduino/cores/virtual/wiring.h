92e8d68028e1ea83d60788552bc83a2fd6b6279d
meanbot <10bec066@nirmauni.ac.in>
2013-07-14 23:37:10 +0530

    Remove analog functions from wiring.h
    This has been deprecated and the prototypes should now be in
    wiring_analog.h

--- libarduino/cores/virtual/wiring.h
+++ libarduino/cores/virtual/wiring.h
@@ -105,10 +105,6 @@ typedef uint8_t byte;
 
 void init(void);
 
-int analogRead(uint8_t);
-void analogReference(uint8_t mode);
-void analogWrite(uint8_t, int);
-
 unsigned long millis(void);
 unsigned long micros(void);
 /* void delay(unsigned long); */
