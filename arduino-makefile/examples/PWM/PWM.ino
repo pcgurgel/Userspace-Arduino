8d731c288be0667f18cac0b3450e05303eeba7c5
David Anders <danders.dev@gmail.com>
2013-07-16 15:53:27 -0500

    tweak to display RGB colors
    this tweak of the example pulses the Red, Green and Blue leds

--- arduino-makefile/examples/PWM/PWM.ino
+++ arduino-makefile/examples/PWM/PWM.ino
@@ -19,6 +19,30 @@ int i=0;
 
 
 for (i = 0 ; i < 41 ; i++ ) {
+		analogWrite(3,(i*500));
+		delay(50);
+}
+
+for (i = 40 ; i > -1 ; i-- ) {
+		analogWrite(3,(i*500));
+		delay(50);
+}
+
+		delay(100);
+
+for (i = 0 ; i < 41 ; i++ ) {
+		analogWrite(5,(i*500));
+		delay(50);
+}
+
+for (i = 40 ; i > -1 ; i-- ) {
+		analogWrite(5,(i*500));
+		delay(50);
+}
+
+		delay(100);
+
+for (i = 0 ; i < 41 ; i++ ) {
 		analogWrite(6,(i*500));
 		delay(50);
 }
@@ -27,5 +51,7 @@ for (i = 40 ; i > -1 ; i-- ) {
 		analogWrite(6,(i*500));
 		delay(50);
 }
+
+		delay(100);
 
 }
