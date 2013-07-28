30fd77af6fdd937e48ce552d604ad2e2998789e6
meanbot <10bec066@nirmauni.ac.in>
2013-07-29 01:25:49 +0530

    Some refactoring in WMath.cpp

--- libarduino/cores/virtual/WMath.cpp
+++ libarduino/cores/virtual/WMath.cpp
@@ -47,7 +47,7 @@ long random(long howsmall, long howbig)
 		return howsmall;
 	}
 	long diff = howbig - howsmall;
-		return random(diff) + howsmall;
+	return random(diff) + howsmall;
 }
 
 long map(long x, long in_min, long in_max, long out_min, long out_max)
@@ -55,5 +55,11 @@ long map(long x, long in_min, long in_max, long out_min, long out_max)
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-unsigned int makeWord(unsigned int w) { return w; }
-unsigned int makeWord(unsigned char h, unsigned char l) { return (h << 8) | l; }
+unsigned int makeWord(unsigned int w)
+{ 
+	return w; 
+}
+unsigned int makeWord(unsigned char h, unsigned char l)
+{ 
+	return (h << 8) | l;
+}
