eecaefb782d9760363f6bb72f7eed4fa8c90f650
Anuj Deshpande <anujdeshpande92@gmail.com>
2013-08-10 15:54:18 +0530

    Add prototypes for SPI functions
    Added SPI.h and SPI.cpp to libarduino/cores/virtual/

---
+++ libarduino/cores/virtual/SPI.cpp
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2013 by Anuj Deshpande <anujdeshpande92@gmail.com>
+ * SPI Library for Userspace Arduino
+ *
+ * This file is free software; you can redistribute it and/or modify
+ * it under the terms of either the GNU General Public License version 2
+ * or the GNU Lesser General Public License version 2.1, both as
+ * published by the Free Software Foundation.
+ */
+#include <stdio.h>
+#include <stdint.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/spi/spidev.h>
+
+SPIClass SPI;
+
+void SPIClass::begin(){
+  printf("Hello\n");
+}
---
+++ libarduino/cores/virtual/SPI.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2013 by Anuj Deshpande <anujdeshpande92@gmail.com>
+ * SPI Library for Userspace Arduino
+ *
+ * This file is free software; you can redistribute it and/or modify
+ * it under the terms of either the GNU General Public License version 2
+ * or the GNU Lesser General Public License version 2.1, both as
+ * published by the Free Software Foundation.
+ */
+#ifndef _SPI_H
+#define _SPI_H
+class SPIClass {
+public:
+		static byte transfer(byte _data);
+		static void begin();
+		static void end();
+		static void setBitOrder(uint8_t);
+		static void setDataMode(uint8_t);
+		static void setClockDivider(uint8_t);
+};
+extern SPIClass SPI;
+#endif
