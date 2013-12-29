7ee687f7eed789f0187f43bd4ed6cbf321cc748d
bugobliterator <siddharthbharatpurohit@gmail.com>
2013-12-29 17:09:44 +0530

    Resolve warnings related to reinitialisation and '|' operator

--- libarduino/libraries/SPI/SPI.cpp
+++ libarduino/libraries/SPI/SPI.cpp
@@ -61,7 +61,7 @@ void SPIClass::setBitOrder(uint8_t bOrder) {
 }
 
 void SPIClass::setDataMode(uint8_t mode) {
-  if( mode == SPI_MODE0 | mode == SPI_MODE1 | mode == SPI_MODE2 | mode == SPI_MODE3)
+  if( (mode == SPI_MODE0) | (mode == SPI_MODE1) | (mode == SPI_MODE2) | (mode == SPI_MODE3))
   {
   	ret = ioctl(fd, SPI_IOC_WR_MODE, &mode);
   		if (ret == -1)
@@ -74,13 +74,13 @@ void SPIClass::setDataMode(uint8_t mode) {
 }
 
 void SPIClass::setClockDivider(uint32_t rate) {
-   if( 	rate == SPI_CLOCK_DIV2 | 
-	rate == SPI_CLOCK_DIV4 | 
-	rate == SPI_CLOCK_DIV16 | 
-	rate == SPI_CLOCK_DIV32 | 
-	rate == SPI_CLOCK_DIV32 | 
-	rate == SPI_CLOCK_DIV64 | 
-	rate == SPI_CLOCK_DIV128
+   if( 	(rate == SPI_CLOCK_DIV2) | 
+	(rate == SPI_CLOCK_DIV4) | 
+	(rate == SPI_CLOCK_DIV16) | 
+	(rate == SPI_CLOCK_DIV32) | 
+	(rate == SPI_CLOCK_DIV32) | 
+	(rate == SPI_CLOCK_DIV64) | 
+	(rate == SPI_CLOCK_DIV128)
 	)										 //changes
   {
   	ret = ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &rate);
